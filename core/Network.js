cba093bf2e8cb5ee2f02b7602fb51ca44b677c94
alec azimuth <azidyn@protonmail.com>
2020-11-20 13:59:13 +0000

    heavy throttling

--- core/Network.js
+++ core/Network.js
@@ -7,8 +7,12 @@ const Packet        = require('./Packet');
 
 let _temp;
 
-const DEF_THROTTLE_ETHUSDT = 25, DEF_THROTTLE_BTCUSDT = 25;
-let _eth_ctr = 0, _btc_ctr = 0;
+const DEF_THROTTLE_ETHUSDT = 15, DEF_THROTTLE_BTCUSDT = 25;
+const DEF_THROTTLE_ALT = 5;
+
+let _eth_ctr = 1, _btc_ctr = 1;
+const _alt_ctr = {};
+
 
 class Network extends EventEmitter {
 
@@ -58,25 +62,33 @@ class Network extends EventEmitter {
             _temp = JSON.parse( msg.data );
 
             /*
-                 Throttle ETH and BTC USDT pairs. Waaaay too many trades for JS garbage collector to
+                 Throttle pairs. Waaaay too many trades for JS garbage collector to
                  handle it's like a fucking firehose.
             */
 
             if ( _temp.s == 'ETHUSDT') 
             {
                 if ( _eth_ctr++ % DEF_THROTTLE_ETHUSDT == 0 ) {
+                    _eth_ctr = 1;
                     this.fire( 'data', _temp.s, Number( _temp.p ) );
                 }
 
             } else if ( _temp.s == 'BTCUSDT')  {
 
                 if ( _btc_ctr++ % DEF_THROTTLE_BTCUSDT == 0 ) {
-                    _btc_ctr = 0;
+                    _btc_ctr = 1;
                     this.fire( 'data', _temp.s, Number( _temp.p ) );
                 }
 
             } else {
-                this.fire( 'data', _temp.s, Number( _temp.p ) );
+
+                if ( !_alt_ctr[ _temp.s ] )
+                    _alt_ctr[ _temp.s ] = 0; // allow first one through
+
+                if ( _alt_ctr[ _temp.s ]++ % DEF_THROTTLE_ALT == 0 ) {
+                    _alt_ctr[ _temp.s ] = 1;
+                    this.fire( 'data', _temp.s, Number( _temp.p ) );
+                }
             }
 
             //  let agg = JSON.parse( msg.data )
